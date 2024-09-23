416ccf68e07ddc45dafac14fd7c4ee24428763b4
Jim124 <fengpingan065@gmail.com>
2024-09-23 11:56:05 +0100

    add header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,12 @@
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className='flex justify-center items-center py-4 bg-slate-800'>
+        <h5 className='text-sm  tracking-wider text-white'>
+          FREE SHIPPING WHEN YOU ORDER 4 OR MORE HIJABS
+        </h5>
+      </div>
+    </header>
+  );
 }
 export default Header;
